9069f8166adf206e2051b586ab52ec90a5e65163
Angel Morales Morales <amorales10@huskers.unl.edu>
2024-04-10 23:27:07 -0500

    Added Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,101 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidMorales()
+        {
+            Assert.AreEqual(1, Program.Subtract("4", "3"));
+            Assert.AreEqual(-1, Program.Subtract("2", "3"));
+            Assert.AreEqual(2, Program.Subtract("8", "6"));
+        }
+
+        [Test]
+        public void Subtract_InvalidMorales()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        public void Subtract_NullMorales()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+
+        [Test]
+        public void Multiply_ValidMorales()
+        {
+            Assert.AreEqual(24, Program.Multiply("6", "4"));
+            Assert.AreEqual(-21, Program.Multiply("-7", "3"));
+            Assert.AreEqual(0, Program.Multiply("0", "20"));
+        }
+
+        [Test]
+        public void Multiply_InvalidMorales()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        public void Multiply_NullMorales()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+
+        [Test]
+        public void Divide_ValidMorales()
+        {
+            Assert.AreEqual(2, Program.Divide("8", "4"));
+            Assert.AreEqual(-2, Program.Divide("-6", "3"));
+            Assert.AreEqual(0, Program.Divide("0", "100"));
+        }
+
+        [Test]
+        public void Divide_InvalidMorales()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        public void Divide_NullMorales()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+
+        [Test]
+        public void Power_ValidMorales()
+        {
+            Assert.AreEqual(8, Program.Power("2", "3"));
+            Assert.AreEqual(25, Program.Power("5", "2"));
+            Assert.AreEqual(1, Program.Power("10", "0"));
+        }
+
+        [Test]
+        public void Power_InvalidMorales()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        public void Power_NullMorlaes()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
+
+
     }
 }
